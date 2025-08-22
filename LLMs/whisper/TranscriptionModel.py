dce15ce020c8c6d54b4f0df1e0726cf631d64df9
ecasallas07 <ecasallas07@gmail.com>
2025-08-21 22:33:54 -0500

    update file for the new model

--- LLMs/whisper/TranscriptionModel.py
+++ LLMs/whisper/TranscriptionModel.py
@@ -1,12 +1,23 @@
 import whisper
+import torch
+from TTS.api import TTS
 
 model = whisper.load_model('base')
-response = model.transcribe('pilar2.mp3', language='es', task='transcribe')
 
-print(response['text'])
+#translate audio file
+# response = model.transcribe('pilar2.mp3', language='es', task='translate')
 
-#The use model whisper
+#transcription audio file - define task 
+reponse_transcription = model.transcribe('pilar2.mp3', language='es', task='transcribe')
+text_transcription = reponse_transcription['text']
 
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(TTS().list_models())
+
+tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
+
+wav = tts.tts(text_transcription, language="en")
+tts.tts_to_file(text_transcription, language="en", file_path="output.wav")
 
 
 
